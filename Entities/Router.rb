c2560e57a0b6aff23fca06dcf77991b7dfa08cd3
FENNEC-LAPTOP\ffene <ffenek@outlook.com>
2021-04-01 13:04:28 +0300

    коррекция router

--- Entities/Router.rb
+++ Entities/Router.rb
@@ -7,14 +7,14 @@ module Resource
 
     loop do
       print 'Выберите команду для взаимодействия с ресурсами (GET/POST/PUT/DELETE) / q для выхода: '
-      verb = gets.chomp
+      verb = gets.chomp.downcase
       break if verb == 'q'
 
       action = nil
 
-      if verb == 'GET'
+      if verb == 'get'
         print 'Для выхода выберите действие (index/show) / q: '
-        action = gets.chomp
+        action = gets.chomp.downcase
         break if action == 'q'
       end
 
@@ -32,23 +32,103 @@ class PostsController
   end
 
   def index
-    puts 'index'
+    @posts.each_with_index do |post, index|
+      puts("#{index} #{post}")
+    end
   end
 
   def show
-    puts 'show'
+    puts('Введите Id')
+    id = gets.to_i
+    if id >= @posts.length
+      puts('Некорентный id')
+    else
+      puts("#{id} #{@posts[id]}")
+    end
   end
 
   def create
-    puts 'create'
+    puts('Введите текст поста')
+    text = gets.chomp
+    @posts.append(text)
+    puts("#{@posts.length - 1} #{text}")
   end
 
   def update
-    puts 'update'
+    puts('Введите id')
+    id = gets.to_i
+    if id >= @posts.length
+      puts('Некорентный id')
+    else
+      puts('Введите текст нового поста')
+      text = gets.chomp
+      @posts[id] = text
+      puts("#{id} #{@posts[id]}")
+    end
   end
 
   def destroy
-    puts 'destroy'
+    puts('Введите id')
+    id = gets.to_i
+    if id >= @posts.length
+      puts('Некорентный id')
+    else
+      @posts.delete_at(id)
+    end
+  end
+end
+
+class CommentsController
+  extend Resource
+
+  def initialize
+    @comments = []
+  end
+
+  def index
+    @comments.each_with_index do |comment, index|
+      puts("#{index} #{comment}")
+    end
+  end
+
+  def show
+    puts('Введите id')
+    id = gets.to_i
+    if id >= @comments.length
+      puts('Некорентный id')
+    else
+      puts("#{id} #{@comments[id]}")
+    end
+  end
+
+  def create
+    puts('Введите комментарий')
+    text = gets.chomp
+    @comments.append(text)
+    puts("#{@comments.length - 1} #{text}")
+  end
+
+  def update
+    puts('Введите id')
+    id = gets.to_i
+    if id >= @comments.length
+      puts('Некорентный id')
+    else
+      puts('Введите новый текст комментария')
+      text = gets.chomp
+      @comments[id] = text
+      puts("#{id} #{@comments[id]}")
+    end
+  end
+
+  def destroy
+    puts('Введите id')
+    id = gets.to_i
+    if id >= @comments.length
+      puts('Некорентный id')
+    else
+      @comments.delete_at(id)
+    end
   end
 end
 
@@ -59,28 +139,30 @@ class Router
 
   def init
     resources(PostsController, 'posts')
+    resources(CommentsController, 'comments')
 
     loop do
       print 'Выберите ресурс, с которым хотите взаимодействовать (1 - Сообщения, 2 - Комментарии, q - Выход): '
-      choise = gets.chomp
+      choise = gets.chomp.downcase
 
       PostsController.connection(@routes['posts']) if choise == '1'
+      CommentsController.connection(@routes['comments']) if choise == '2'
       break if choise == 'q'
     end
 
     puts 'До свидания!'
   end
-  call
+
   def resources(klass, keyword)
     controller = klass.new
     @routes[keyword] = {
-      'GET' => {
+      'get' => {
         'index' => controller.method(:index),
         'show' => controller.method(:show)
       },
-      'POST' => controller.method(:create),
-      'PUT' => controller.method(:update),
-      'DELETE' => controller.method(:destroy)
+      'post' => controller.method(:create),
+      'put' => controller.method(:update),
+      'delete' => controller.method(:destroy)
     }
   end
 end
